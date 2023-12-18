ae46a6fc0202ed67ee83372b17e0fd1243b71cfd
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2023-12-18 03:00:50 +0200

    initial tests pass

--- backend/userRoutes.js
+++ backend/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { signup, login, updateProfile } = require("./userController");
+const verifyToken = require("./authMiddleware");
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.post("/update-profile", updateProfile);
+router.patch("/update-profile", verifyToken, updateProfile);
 
 module.exports = router;
