b1ec427e8d8463080acc58efe8074504cd16745a
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2023-12-20 13:52:31 +0200

    Login Done

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,30 @@
+// server.js or app.js
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
 
 require("./connection");
-
+const authenticateToken = require("./authMiddleware");
 const userRoutes = require("./userRoutes");
 const surveyRoutes = require("./surveyRoutes");
+// Import the middleware
 
 // Middleware to handle errors
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something went wrong!");
 });
-app.use(cors());
+
 app.use(express.json());
+app.use(cors());
+
+// Apply the middleware to the route
+app.get("/survey/list", authenticateToken, (req, res) => {
+  // Your existing code to fetch and send surveys
+});
+
+// Your existing user and survey routes
 app.use("/user", userRoutes);
 app.use("/survey", surveyRoutes);
 
--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [surveys, setSurveys] = useState([]);
+  const navigate = useNavigate();
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -23,21 +26,33 @@ const Login = () => {
         password,
       });
 
+      console.log("Login Response:", response);
+
       const token = response.data.token;
+
+      console.log("Token:", token);
 
       localStorage.setItem("token", token);
 
-      // Fetch and set the list of surveys upon successful login
-      const surveysResponse = await axios.get(
-        "http://localhost:3001/survey/list",
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
+      navigate("/dashboard");
 
-      setSurveys(surveysResponse.data.surveys);
+      // Fetch and set the list of surveys upon successful login
+      // try {
+      //   const surveysResponse = await axios.get(
+      //     "http://localhost:3001/survey/list",
+      //     {
+      //       headers: {
+      //         Authorization: `Bearer ${token}`,
+      //       },
+      //     }
+      //   );
+
+      //   console.log("Surveys Response:", surveysResponse.data);
+
+      //   setSurveys(surveysResponse.data.surveys);
+      // } catch (surveysError) {
+      //   console.error("Error fetching surveys:", surveysError);
+      // }
     } catch (error) {
       console.error("Login failed", error.response.data.message);
     }
